6089c3a4f36bbe9d1c85e1e15d83c2d1344b615c
saulfabreg Wii VC Project <saulfabregamboa@outlook.com>
2021-08-02 20:49:00 -0500

    Add files via upload

---
+++ source/fceux/version.h
@@ -0,0 +1,75 @@
+/* FCE Ultra - NES/Famicom Emulator
+ *
+ * Copyright notice for this file:
+ *  Copyright (C) 2001 Aaron Oneal
+ *  Copyright (C) 2002 Xodnizel
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+//#define PUBLIC_RELEASE		// uncomment this when making a public release, but comment back before committing
+
+#ifndef __FCEU_VERSION
+#define __FCEU_VERSION
+
+//todo - everyone will want to support this eventually, i suppose
+#if  defined(_MSC_VER) && !defined(__QT_DRIVER__)
+#include "scmrev.h"
+#else
+#ifdef SVN_REV
+#define SCM_REV_STR SCM_REV
+#else
+#define SCM_REV_STR ""
+#endif
+#endif
+
+#define FCEU_NAME "FCEUX"
+
+#define FCEU_FEATURE_STRING ""
+
+#ifdef _DEBUG
+#define FCEU_SUBVERSION_STRING " debug"
+#elif defined(PUBLIC_RELEASE)
+#define FCEU_SUBVERSION_STRING ""
+#else
+#define FCEU_SUBVERSION_STRING "-interim git" SCM_REV_STR
+#endif
+
+#if defined(_MSC_VER)
+#define FCEU_COMPILER ""
+#define FCEU_COMPILER_DETAIL " msvc " _Py_STRINGIZE(_MSC_VER)
+#define _Py_STRINGIZE(X) _Py_STRINGIZE1((X))
+#define _Py_STRINGIZE1(X) _Py_STRINGIZE2 ## X
+#define _Py_STRINGIZE2(X) #X
+//re: http://72.14.203.104/search?q=cache:HG-okth5NGkJ:mail.python.org/pipermail/python-checkins/2002-November/030704.html+_msc_ver+compiler+version+string&hl=en&gl=us&ct=clnk&cd=5
+#else
+// TODO: make for others compilers
+#define FCEU_COMPILER ""
+#define FCEU_COMPILER_DETAIL ""
+#endif
+
+#define FCEU_VERSION_MAJOR  2
+#define FCEU_VERSION_MINOR  3
+#define FCEU_VERSION_PATCH  0
+
+#define FCEU_VERSION_NUMERIC  ( (FCEU_VERSION_MAJOR*10000) + (FCEU_VERSION_MINOR*100) + (FCEU_VERSION_PATCH) )
+#define FCEU_VERSION_MAJOR_DECODE(x)  ( (x / 10000) )
+#define FCEU_VERSION_MINOR_DECODE(x)  ( (x / 100) % 100 )
+#define FCEU_VERSION_PATCH_DECODE(x)    (x % 100)
+
+#define FCEU_VERSION_STRING "2.3.0"  FCEU_SUBVERSION_STRING FCEU_FEATURE_STRING FCEU_COMPILER
+#define FCEU_NAME_AND_VERSION FCEU_NAME " " FCEU_VERSION_STRING
+
+#endif
---
+++ source/fceux/x6502abbrev.h
@@ -0,0 +1,26 @@
+#ifndef _X6502ABBREV_H_
+#define _X6502ABBREV_H_
+
+//include this file LAST, or else the #defines will overwrite CRT and STL symbols
+
+#ifdef GEKKO
+#undef _X
+#undef _S
+#undef _P
+#endif
+
+#define _PC        X.PC
+#define _A         X.A
+#define _X         X.X
+#define _Y         X.Y
+#define _S         X.S
+#define _P         X.P
+#define _PI        X.mooPI
+#define _DB        X.DB
+#define _count     X.count
+#define _tcount    X.tcount
+#define _IRQlow    X.IRQlow
+#define _jammed    X.jammed
+
+
+#endif
